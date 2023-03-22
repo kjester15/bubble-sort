1db26363ab54ff450e7dbda5d1edd689833e4688
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-03-22 15:30:44 -0600

    Complete bubble sort project

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,3 +1,21 @@
 def bubble_sort(array)
-
+    length = array.length
+    loop do
+        swapped = false
+        (length-1).times do |x|
+            if array[x] > array[x+1]
+                placeholder = array[x]
+                array[x] = array[x+1]
+                array[x+1] = placeholder    
+                swapped = true
+            end
+        end
+        length = length - 1
+        break if swapped == false
+    end
+    return array.to_s
 end
+
+data = [9,-1,7,3,0,2]
+
+puts bubble_sort(data)
